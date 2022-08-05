d6f0008632cdcc420e900533e6be1dc87ce20f14
JESUS-K <kerlyjesus98@outlook.com>
2022-08-05 09:23:30 -0500

    rama nueva JesusK

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php
 
 	//define("BASE_URL", "http://localhost/erp-seuat-v1/");
-	const BASE_URL = "http://localhost:8012/erp-prueba";
+	const BASE_URL = "http://localhost/erp-seuat-v5";
 	const VERSION_NAME = "v1.0.2Alpha";
 
 	//Zona horaria
@@ -10,8 +10,8 @@
 	//const VIEWS = "Views/";
 	const DB_HOST = "10.10.124.26";
 	const DB_NAME = "erpseuat";
-	const DB_USER = "usr_seuat";
-	const DB_PASSWORD = "seuat21";
+	const DB_USER = "usr_seuat_practicante";
+	const DB_PASSWORD = "seuat.2022";
 	const DB_CHARSET = "utf8";
 
 	//Delimitadores decimal y millar Ej. 27,1985.00
